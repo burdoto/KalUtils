c20c411dde51a1e87298828a2d0a66268812f2a5
burdoto-home <endermanialp@gmail.com>
2018-11-08 18:49:51 +0100

    Code adjustments

---
+++ src/main/java/de/kaleidox/util/helpers/NullHelper.java
@@ -0,0 +1,21 @@
+package de.kaleidox.util.helpers;
+
+import java.util.Objects;
+
+public class NullHelper {
+    public static <T> T orDefault(T item, T def) {
+        requireNonNull(item, def);
+        return (item == null ? def : item);
+    }
+
+    public static void requireNonNull(Object... items) {
+        for (Object item : items) {
+            Objects.requireNonNull(item);
+        }
+    }
+
+    protected static <T> Object requireNonNullElse(T test, T orElse) {
+        if (Objects.isNull(test)) return orElse;
+        return test;
+    }
+}
---
+++ src/main/java/de/kaleidox/util/objects/markers/DoublePartObject.java
@@ -0,0 +1,23 @@
+package de.kaleidox.util.objects.markers;
+
+public class DoublePartObject<A, B> {
+    private A partA;
+    private B partB;
+
+    public DoublePartObject(A a, B b) {
+        partA = a;
+        partB = b;
+    }
+
+    public static <A, B> DoublePartObject<A, B> of(A a, B b) {
+        return new DoublePartObject<>(a, b);
+    }
+
+    public A getA() {
+        return partA;
+    }
+
+    public B getB() {
+        return partB;
+    }
+}
